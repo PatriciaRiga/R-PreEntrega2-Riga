2016171bf4ed59249496554a928072942d6ebcc3
Patricia Riga <patoriga51@gmail.com>
2023-09-05 09:40:19 -0300

    Sincronizando contador

--- src/components/ItemDetailCard/ItemDetailCard.jsx
+++ src/components/ItemDetailCard/ItemDetailCard.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 import "./ItemDetailCard.css";
+import { Link } from "react-router-dom";
 
 const ItemDetailCard = ({ name, img, price, description, category, stock }) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
+ 
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+    }
 
     return (
-
         <div className="prodContainer">
             <div className="divProd">
                 <h3 className="nombreProd">{name}</h3>
@@ -13,7 +18,12 @@ const ItemDetailCard = ({ name, img, price, description, category, stock }) => {
                 <h4 className="description">Categoría: {category}</h4>
                 <p className="precio">Precio: ${price}</p>
                 <p className="description">{description}</p>
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log("cantidad agregada " + quantity)} />
+                {
+                    quantityAdded > 0 ? (
+                        <Link to='/cart' className="finCompra">Finalizar compra</Link>
+                    ) : (
+                        <ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />  
+                    )}
             </div>
         </div>
     );
